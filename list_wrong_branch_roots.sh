5991084e44fd5a297eae2109a9c8aeae988a0990
Ulrich Spörlein <uqs@FreeBSD.org>
2020-04-25 17:59:47 +0200

    Unbreak the script, as a too long command wont get proper argument substitution.

--- list_wrong_branch_roots.sh
+++ list_wrong_branch_roots.sh
@@ -3,7 +3,10 @@
 n=`sysctl -n hw.ncpu`
 n=${n:-4}
 
-git rev-list --all | xargs -n1 -P$n -I@ sh -c '
-git ls-tree -r @ | grep -q "[[:space:]]head/" && { echo -n "head/ "; git log -n1 --pretty=format:"%h %ad %N" @; };
-git ls-tree -r @ | grep -q "[[:space:]]dist/" && { echo -n "dist/ "; git log -n1 --pretty=format:"%h %ad %N" @; };
+# ACHTUNG: Do not make the string passed to the shell any longer, because that
+# will stop it from working. Not sure why though, the "@" then no longer gets
+# replaced.
+git rev-list --all --abbrev-commit | xargs -n1 -P$n -I@ sh -c 'x=@;
+git ls-tree -r $x|grep -q "	head/" && { echo -n "h "; git log -n1 --pretty=format:"%h %ad %N" $x; };
+git ls-tree -r $x|grep -q "	dist/" && { echo -n "d "; git log -n1 --pretty=format:"%h %ad %N" $x; };
 ' | sed '/^$/d'
