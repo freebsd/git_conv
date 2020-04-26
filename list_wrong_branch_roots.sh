e71298e17ea5cd4c155d974e19a0c32e74759198
Ulrich Spörlein <uqs@FreeBSD.org>
2020-04-26 11:57:42 +0200

    Fix remaing vendor branch tag nonsense
    This fixes issue #5, and was helped by speeding up the script to not do
    recursive ls-tree and also do only half of them.

--- list_wrong_branch_roots.sh
+++ list_wrong_branch_roots.sh
@@ -1,12 +1,17 @@
 #!/bin/sh
 
-n=`sysctl -n hw.ncpu`
+n=`sysctl -n hw.ncpu 2>/dev/null`
 n=${n:-4}
 
-# ACHTUNG: Do not make the string passed to the shell any longer, because that
-# will stop it from working. Not sure why though, the "@" then no longer gets
-# replaced.
-git rev-list --all --abbrev-commit | xargs -n1 -P$n -I@ sh -c 'x=@;
-git ls-tree -r $x|grep -q "	head/" && { echo -n "h "; git log -n1 --pretty=format:"%h %ad %N" $x; };
-git ls-tree -r $x|grep -q "	dist/" && { echo -n "d "; git log -n1 --pretty=format:"%h %ad %N" $x; };
-' | sed '/^$/d'
+trap 'rm -f "$TMPFILE"' EXIT
+TMPFILE=$(mktemp -t `basename $0`) || exit 1
+
+cat > $TMPFILE << EOS
+#!/bin/sh
+git ls-tree "\$@" | grep -Eq "	(head|dist)(/|$)" && {
+    git ls-tree "\$@" | grep -Eq "	head(/|$)" && { echo -n "h "; git log -n1 --pretty=format:"%h %ad %N" "\$@"; };
+    git ls-tree "\$@" | grep -Eq "	dist(/|$)" && { echo -n "d "; git log -n1 --pretty=format:"%h %ad %N" "\$@"; };
+};
+EOS
+
+git rev-list --all --abbrev-commit | xargs -n1 -P$n  sh $TMPFILE | sed '/^$/d'
