3556248a13ff04502e66d138ffdf91979e22bd79
Ulrich Spörlein <uqs@FreeBSD.org>
2020-05-12 11:16:02 +0200

    Update the comparing script to work with flattened/unflattened tags
    Skip a bunch of known-differing tags and not-handled cases, also
    fixup 2 typos in the rules that this comparison uncovered so far.

--- compare_branches.sh
+++ compare_branches.sh
@@ -19,7 +19,7 @@ mkdir -p scratch
 oldmd0=`mount -ptufs | awk -vS=$S '$2 = S {print $1; exit}'`
 case "$oldmd0" in
     /dev/md*)
-        sudo umount $S
+        sudo umount -f $S
         sudo mdconfig -d -u $oldmd0
         ;;
 esac
@@ -58,25 +58,96 @@ diff_it() {
     svn export --ignore-keywords -q $SVN/$from s &
     GIT_DIR=$GIT git archive --format=tar --prefix=g/ $to | tar xf -
     wait
-    diff -ruN s g || { echo "diffs found in SVN $from vs git $to, bailing out" >&2; exit 1; }
+    test -d s || exit 1
+    test -d g || exit 1
+    diff -ruN -I'[$]FreeBSD.*[$]' s g >/dev/null || {
+        # we don't flatten tags, so try 1 or 2 levels deeper again.
+        if [ 1 -eq `ls -1 g/|wc -l` ] ; then
+            diff -ruN -I'[$]FreeBSD.*[$]' s g/*/ >/dev/null || {
+                if [ 1 -eq `ls -1 g/*/|wc -l` ] ; then
+                    diff -ruN -I'[$]FreeBSD.*[$]' s g/*/*/ >/dev/null || {
+                        echo "diffs found in SVN $from vs git $to, bailing out" >&2; exit 1;
+                    }
+                fi
+            }
+        else
+            echo "diffs found in SVN $from vs git $to, bailing out" >&2; exit 1;
+        fi
+    }
     touch "$sentinel"
     cd ..
     set +e
 }
 
+if [ $# -ge 2 ] ; then
+    while [ $# -ge 2 ] ; do
+        from=$1; shift
+        to=$1;shift
+        diff_it $from $to
+    done
+else
+
+#diff_it vendor-cddl/opensolaris/dist:178529 275928fc142
+#diff_it vendor-cddl/opensolaris/20080410a:179530 275928fc142
+#diff_it vendor-sys/opensolaris/dist:194446 vendor/opensolaris/dist
+#diff_it vendor-sys/illumos/dist:238570 vendor/illumos/dist
+#diff_it vendor-crypto/openssh/dist vendor/openssh/dist
+#diff_it vendor-crypto/openssh/5.2p1 vendor/openssh/5.2p1
+
 diff_it head master
 
 case "$type" in
     base)
+        diff_it releng/ALPHA_2_0 releng/2.0a
+        diff_it releng/BETA_2_0 releng/2.0b
         for t in release stable releng; do
-            for b in `svn ls $SVN/$t | grep -v A | fgrep -v 2.1.6.1`; do
+            for b in `svn ls $SVN/$t | grep -v A_2_0`; do
                 diff_it $t/$b
             done
         done
         for t in vendor; do
-            for b in `svn ls $SVN/$t | egrep "tcsh"`; do
-                for s in `svn ls $SVN/$t/$b`; do
+            for b in `svn ls $SVN/$t`; do
+                for s in `svn ls $SVN/$t/$b | grep '/$'`; do
+                    case "$b$s" in
+                        Juniper/libxo/|NetBSD/blacklist/|NetBSD/bmake/|NetBSD/libc-pwcache/|NetBSD/libc-vis/|NetBSD/libedit/|NetBSD/libexecinfo/|NetBSD/lukemftp/|NetBSD/lukemftpd/|NetBSD/mknod/|NetBSD/mtree/|NetBSD/softfloat/|NetBSD/sort/|NetBSD/tests/|NetBSD/unvis/|NetBSD/vis/|NetBSD/xlint/)
+                            for r in `svn ls $SVN/$t/$b$s | grep '/$'`; do
+                                case "$r" in
+                                    bmake-20121111/) continue ;;
+                                esac
+                                diff_it $t/$b$s$r
+                            done
+                            continue
+                            ;;
+                    esac
+                    s=${s%/}
+                    case "$b$s" in
+                        ipfilter/*) continue ;;
+                    esac
                     diff_it $t/$b$s
+                done
+            done
+        done
+        for t in vendor-crypto vendor-sys; do
+            for b in `svn ls $SVN/$t`; do
+                for s in `svn ls $SVN/$t/$b`; do
+                    s=${s%/}
+                    case "$b$s" in
+                        # bunch of extra files in here, as we didn't propagate the deletes, we could compare to an older rev though.
+                        openssh/4.6p1|openssh/4.7p1|openssh/4.9p1|openssh/5.0p1|openssh/5.1p1|openssh/5.9p1) continue ;;
+                        # I thought the prefixes were collapsed, TODO fix the rule stripping to fix this up
+                        telnet/95-10-23|telnet/dist) continue ;;
+                        # unflattened tags, 2010 and following are identical again
+                        acpica/200*) continue ;;
+                        # compare against pre-flattening
+                        ath/0.9.16*) diff_it $t/$b$s:182296 vendor/$b$s; continue ;;
+                        # FIXME
+                        ath/0*) continue ;;
+                        # need to compare 2 branches, not implemented yet but was compared manually
+                        illumos/*|ngatm/*|opensolaris/*|pf/*) continue ;;
+                        # TODO merge or compare against vendor-sys
+                        ipfilter/*) continue ;;
+                    esac
+                    diff_it $t/$b$s vendor/$b$s
                 done
             done
         done
@@ -121,3 +192,4 @@ case "$type" in
         #done
         ;;
 esac
+fi
