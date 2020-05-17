5713bc4340475aa05594defbd91c614c321fb36b
Ulrich Spörlein <uqs@FreeBSD.org>
2020-05-17 20:44:59 +0200

    Lighten the load on my SSD by using a tmpfs
    And also special case the 11.0-RELEASE which got tagged as 11.0.1
    
    Releases after 6.0 basically have no meaningful diff (aside from
    newvers.sh and the beastie image). Releases in 5.x have some ppp diff,
    and of course there are the CVS Id changes all around.

--- compare_archive_tags.sh
+++ compare_archive_tags.sh
@@ -3,6 +3,24 @@
 : ${BASE=${PWD}}
 REPO=$BASE/freebsd-base.git
 
+S=$PWD/scratch
+mkdir -p scratch
+# cleanup previous runs
+oldmd0=`mount -ptufs | awk -vS=$S '$2 = S {print $1; exit}'`
+case "$oldmd0" in
+    /dev/md*)
+        sudo umount -f $S
+        sudo mdconfig -d -u $oldmd0
+        ;;
+esac
+
+set -e
+md0=`sudo mdconfig -a -t swap -s 12G -S 4096 -o compress`
+sudo newfs -U $md0 >/dev/null
+
+sudo mount -o async,noatime /dev/$md0 $S
+sudo chown $USER $S
+
 if [ "$#" -ne 1 ]; then
     #1.0-RELEASE     # nope
     #2.1.7.1-RELEASE # we don't have the CVS or SVN tag for it?
@@ -76,19 +94,28 @@ set -- \
 fi
 
 for r; do
-    rm -rf a.git b.git
+    rm -rf $S/a.git $S/b.git
     extra_flags=
     case $r in
         *.*.*-RELEASE) tag=${r%-RELEASE} ;;
         *.*-RELEASE) tag=${r%-RELEASE}.0 ;;
     esac
+    test -r diff_$tag && continue
     case $tag in
         2.*) extra_flags='-x eBones -x secure' ;;
         3.*) extra_flags='-x secure -x crypto -x kerberosIV' ;;
         4.*) extra_flags='-x crypto -x kerberosIV -x kerberos5 -x secure' ;;
         5.*) extra_flags='-x crypto -x kerberos5 -x secure -x .cvsignore' ;;
     esac
-    git clone -b release/$tag $REPO a.git
-    git clone -b release/${tag}_shipped $REPO b.git
-    diff -ruN -I'[$](Header|Id|FreeBSD).*[$]' -I',v [0-9].* Exp( .LBL.)?[->";)*/ ]*.?$' -x .git `echo $extra_flags` a.git b.git || exit 1
+    case $r in
+        11.0-RELEASE)
+            git clone -b release/11.0.1 $REPO $S/a.git 2>/dev/null
+            git clone -b release/${tag}_shipped $REPO $S/b.git 2>/dev/null
+            ;;
+        *)
+            git clone -b release/$tag $REPO $S/a.git 2>/dev/null
+            git clone -b release/${tag}_shipped $REPO $S/b.git 2>/dev/null
+            ;;
+    esac
+    diff -ruN -I'[$](Header|Id|FreeBSD).*[$]' -I',v [0-9].* Exp( .LBL.)?[->";)*/ ]*.?$' -x .git -x .cvsignore `echo $extra_flags` $S/a.git $S/b.git | tee diff_$tag
 done
