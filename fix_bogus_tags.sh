6c869c026b5cb0ed28a75ca5123fcda368b40477
Ulrich Spörlein <uqs@FreeBSD.org>
2020-06-21 12:05:57 +0200

    Add some tags that will need manual fixes

--- fix_bogus_tags.sh
+++ fix_bogus_tags.sh
@@ -10,8 +10,9 @@ export GIT_DIR=$git
 
 rewrite_tag()
 {
-    local tag
+    local tag target
     tag=$1
+    target=${tag}~1
 
     c_auth=`git cat-file tag $tag | sed -n '/^tagger/s/^tagger //; s/ [0-9 +]*$//p'`
     c_date=`git cat-file tag $tag | sed -n '/^tagger/s/^tagger //p' | egrep -o '[0-9]* [+0-9]*$'`
@@ -22,7 +23,7 @@ rewrite_tag()
     old_commit=`git show -s --format=%h "$tag^{commit}"`
 
     # Move the tag up to the ancestor
-    GIT_COMMITTER_DATE="$c_date" GIT_COMMITTER_NAME="$c_committer" GIT_COMMITTER_EMAIL="$c_email" git tag -a -f -m "$c_msg" ${tag} ${tag}~1
+    GIT_COMMITTER_DATE="$c_date" GIT_COMMITTER_NAME="$c_committer" GIT_COMMITTER_EMAIL="$c_email" git tag -a -f -m "$c_msg" ${tag} ${target}
     # NOTE: grabs only the 2nd line (with the tag) and then re-edits the note
     # to drop the extra newline in the middle.
     git notes append -m "`git notes show $old_commit|tail -1`" "$tag^{commit}"
@@ -41,3 +42,14 @@ git --git-dir=$git for-each-ref --format='%(refname:short)' refs/tags |
             rewrite_tag $rev
         fi
     done
+
+# r221422 tagged a single file, as the vendor tree just has a single file. This
+# is however a file copy, not a dir copy and we end up with an extra commit
+# object. Patch it up.
+if git rev-parse -q --verify vendor/v4l/2.6.17~1 >/dev/null; then
+    # v4l had 1 file imported, it was tagged, later a 2nd file was added and
+    # the tag was moved. The 2nd tag only copies the 2nd file though, so we
+    # cannot just ignore the first tag.
+    echo rewrite_tag vendor/v4l/2.6.17
+    echo rewrite_tag vendor/illumos/20120614 to revision=238587
+fi
