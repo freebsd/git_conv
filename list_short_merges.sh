e565f0b9eb83fa37c225f19b2069d9bd41d6adde
Ulrich Spörlein <uqs@FreeBSD.org>
2020-06-28 18:14:54 +0200

    List more empty commits we might want to skip and fix printing of the merges of stub branches.

--- list_short_merges.sh
+++ list_short_merges.sh
@@ -2,6 +2,7 @@
 
 git=${1:-freebsd-base.git}
 export GIT_DIR=$git
+test $# -ge 1 && shift
 
 # Show merge commits onto mainline, where one of the parents is a single lone
 # commit. This is often caused by cvs2svn and we could inline these commits
@@ -20,13 +21,13 @@ print_commit()
 
     # TODO: sometimes the parent might just have 1 other child, namely r13122 (but possibly others)
     echo $children | grep -q $c || { echo "child <-> parent doesn't match, scripting error?" >&2; exit 1; }
-    if [ `echo $children | wc -l` = 1 ]; then
+    if [ `echo "$children" | wc -l` = 1 ]; then
 	echo -n "$c is merge commit with short parent: $p  notes: "
 	echo `git log -n1 --format=%N $c` ← `git log -n1 --format=%N $p`
 	# assuming that $c is on master
-	svn_path=`git log --format=%N $p | egrep -o 'path=/[^;]*'`
+	svn_path=`git log --format=%N $p | head -1 | egrep -o 'path=/[^;]*'`
 	svn_path=${svn_path#path=}
-	svn_rev=`git log --format=%N $p | egrep -o 'revision=[0-9]*'`
+	svn_rev=`git log --format=%N $p | head -1 | egrep -o 'revision=[0-9]*'`
 	svn_rev=${svn_rev#revision=}
 cat << EOS
 match $svn_path
@@ -39,7 +40,7 @@ EOS
     fi
 }
 
-git log --format='%H %P' --all --reverse | awk '{if (NF == 3) { print NF " " $0}}' |
+git log --format='%H %P' --all --reverse "$@" | awk '{if (NF == 3) { print NF " " $0}}' |
     while read n c p1 p2; do
 	# TODO skip commits on /projects et al
 	git rev-parse -q --verify $p1\~1 > /dev/null || print_commit $c $p1
