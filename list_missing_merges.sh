1667926747c4ad2c55ae59cb7007c5d4b4e06294
Ulrich Spörlein <uqs@FreeBSD.org>
2020-07-25 21:46:21 +0200

    Just print vendor tips and all tags that are not merged to main.

--- list_missing_merges.sh
+++ list_missing_merges.sh
@@ -6,7 +6,7 @@ branch=${3:-master}
 export GIT_DIR=$git
 
 # Print the potential MFV merge commits that have only a single parent
-git log --pretty=format:'%h %p %ad %N' --grep="^MF[VPp]" --max-parents=1 master $branch | sed '/^$/d'
+#git log --pretty=format:'%h %p %ad %N' --grep="^MF[VPp]" --max-parents=1 master $branch | sed '/^$/d'
 
 # Alternative approach, list all vendor/foo/dist refs that are *not* reachable
 # from master. These are changed vendor imports that a) haven't been recorded
@@ -25,11 +25,11 @@ git log --pretty=format:'%h %p %ad %N' --grep="^MF[VPp]" --max-parents=1 master 
 #
 # We don't want the 2.4 tag to stick out, it wouldn't show up when just logging /dist.
 
-# TODO: is this maybe just `git tag --no-merged`?
 git show-ref dist | sed 's,.* refs/heads/,,; s,/dist,,' | while read v; do
   git show-ref --heads --tags | fgrep $v | while read sha ref; do
       if ! git --git-dir=$git merge-base --is-ancestor $sha $branch; then
-	  echo "WARNING: $ref isn't reachable from $branch"
+	  #echo "WARNING: $ref isn't reachable from $branch"
+	  echo ${ref}
       fi
   done
 done
@@ -38,4 +38,4 @@ done
 # Show un-merged vendor tags, these need manual inspection for why they were
 # not properly recorded, often this is because the tag was cvs2svn manufactured
 # :/
-git tag --sort=-taggerdate --no-merged master vendor/\*
+git tag --sort=-taggerdate --no-merged master vendor/\* | sed 's,^,refs/tags/,' #while read ref; do git show -s $ref; done
